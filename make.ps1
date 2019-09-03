bdb715c4bb8e4544de0119bc606d2ffef7f84944
manikarnam <49348793+manikarnam@users.noreply.github.com>
2019-09-03 18:39:28 +0530

    dd

--- make.ps1
+++
@@ -1,5 +0,0 @@
-if([System.String]::IsNullOrWhiteSpace($env:IMAGE_NAME)) {
-    $env:IMAGE_NAME='jenkins4eval/jnlp-agent:test-windows'
-}
-
-& docker build -f Dockerfile-windows -t $env:IMAGE_NAME .
